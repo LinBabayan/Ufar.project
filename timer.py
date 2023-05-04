6062e81aae18d2beb71ee3bd116c6b6de9635f57
LinBabayan <128178474+LinBabayan@users.noreply.github.com>
2023-05-04 16:05:18 +0400

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,21 +1,18 @@
 import time
-from datetime import timedelta
 
 while True:
-    try:
-        my_time = int(input("Enter time in seconds: "))
-        if my_time > 0:
-            break
-        else:
-            print("Invalid input. Please enter a positive number.")
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
+    my_time = input("Enter time in seconds: ")
+    if my_time.isdigit() and int(my_time) > 0:
+        break
+    print("Invalid input. Please enter a positive integer.")
 
-time_delta = timedelta(seconds=my_time)
+my_time = int(my_time)
 
-for delta in reversed(range(my_time)):
-    time_left = str(time_delta - timedelta(seconds=delta))
-    print(time_left[-8:])
+for x in reversed(range(my_time + 1)):
+    seconds = x % 60
+    minutes = (x // 60) % 60
+    hours = x // 3600
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("TIME IS UP!")
